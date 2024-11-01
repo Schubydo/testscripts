21b4db86a8a41c89c1ccb981ce93d5dc924b58ce
Schubydo <dylanschubert@rocketmail.com>
2024-11-01 12:34:41 +0000

    updates

--- endswith.py
+++ endswith.py
@@ -7,12 +7,16 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get("your_url_here")
 
+# Set the unique identifier for the parent node
+parent_id = "dijit__TreeNode_3"
+
 # Wait until the parent tree node is loaded
 wait = WebDriverWait(driver, 10)
-parent_tree_node = wait.until(EC.presence_of_element_located((By.XPATH, "your_parent_node_xpath_here")))
+parent_tree_node = wait.until(EC.presence_of_element_located((By.ID, parent_id)))
 
-# Now find all descendant nodes with the 'dijit_tree_node' class within this parent node
-child_nodes = parent_tree_node.find_elements(By.XPATH, ".//div[contains(@class, 'dijit_tree_node')]")
+# Find all child nodes with an ID pattern starting with 'dijit__TreeNode_' under the parent
+# Assuming they are direct children or descendants
+child_nodes = parent_tree_node.find_elements(By.XPATH, ".//div[starts-with(@id, 'dijit__TreeNode_') and not(@id='" + parent_id + "')]")
 
 # Print the number of child nodes
 print("Number of child nodes:", len(child_nodes))
